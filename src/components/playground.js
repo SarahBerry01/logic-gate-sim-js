556c97880d3dac72b3ddc65e2b38e87a9200a2e3
SarahBerry01 <sarah@berrys.me.uk>
2023-09-14 14:16:27 +0100

    initial experiment

--- src/components/playground.js
+++ src/components/playground.js
@@ -1,9 +1,46 @@
-import React from "react";
+import React, { useState } from 'react';
+
 
 function Playground() {
-    return <div className="Playground">
-        
-    </div>
+
+    const [gates, setGates] = useState([]);
+    class Gate {
+        constructor(type) {
+          this.type = type
+        }
+      }
+    
+    function GatePicker() {
+        return <div className="GatePicker">
+        <button className="GateButton" onClick={() => addGate({ type: "and" })}>AND</button>
+        <button className="GateButton" onClick={() => addGate({ type: "not" })}>NOT</button>
+        <button className="GateButton" onClick={() => addGate({ type: "or" })}>OR</button>
+        </div>
+    }
+    function Canvas(){
+        return <div className="Canvas">
+        </div>
+    }
+    
+    function Toolbar() {
+        return <div className="Toolbar">
+        </div>
+    }
+    
+    function addGate(props) {
+        const newGate = new Gate(props.type)
+        console.log(props.type)
+        console.log(gates)
+        setGates([...gates, newGate]);
+      };
+
+      
+    return(
+    <div className='Playground'>
+        <GatePicker/>
+        <Canvas/>
+        <Toolbar/>
+      </div>)
 }
 
 export default Playground;
