8b4252ecf9168e4c0400cc34199dfe36904fbfc4
SarahBerry01 <sarah@berrys.me.uk>
2023-09-16 11:39:27 +0100

    only move gate when move tool selected

--- src/components/playground.js
+++ src/components/playground.js
@@ -1,6 +1,5 @@
-import React, { useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import interact from 'interactjs';
-import and from '../assets/and.svg'
 
 
 function Playground() {
@@ -11,22 +10,22 @@ function Playground() {
     setSelectedTool(buttonId);
   };
 
-    function Canvas(){
-        return <div className="Canvas" id="Canvas">
-        <svg src="../assets/and.svg"/>
-        {gates.map((gate) => (
-          <div
-            key={gate.id}
-            id={gate.id}
-            className={`gate ${gate.type}`}
-            x={gate.x}
-            y={gate.y}
-            style={{ transform: `translate(${gate.x}px, ${gate.y}px)` }}
-          ></div>
-        ))}
-      </div>
+  function Canvas() {
+    return <div className="Canvas" id="Canvas">
+      <svg src="../assets/and.svg" />
+      {gates.map((gate) => (
+        <div
+          key={gate.id}
+          id={gate.id}
+          className={`gate ${gate.type}`}
+          x={gate.x}
+          y={gate.y}
+          style={{ transform: `translate(${gate.x}px, ${gate.y}px)` }}
+        ></div>
+      ))}
+    </div>
 
-    }
+  }
 
   const addGate = (gateType) => {
     const newGate = {
@@ -40,34 +39,34 @@ function Playground() {
   };
 
 
-useEffect(() => {
+  useEffect(() => {
 
 
-  gates.forEach((gate) => {
-    const element = document.getElementById(gate.id);
+    gates.forEach((gate) => {
+      const element = document.getElementById(gate.id);
 
-    if (element) {
-      interact(element).draggable({
-        modifiers: [
-          interact.modifiers.restrictRect({
-            restriction: 'parent'
-          })
-        ],
-        onmove: (event) => {
-          const target = event.target;
-          const x = parseFloat(target.getAttribute('x')) || 0;
-          const y = parseFloat(target.getAttribute('y')) || 0;
-  
-          target.style.transform = `translate(${x + event.dx}px, ${y + event.dy}px)`;
-          target.setAttribute('x', x + event.dx);
-          target.setAttribute('y', y + event.dy);
-          gate.x = x + event.dx
-          gate.y = y + event.dy
-        },
-      });
-    }
-  });
-}, [gates]);
+      if (element && selectedTool === 'move') {
+        interact(element).draggable({
+          modifiers: [
+            interact.modifiers.restrictRect({
+              restriction: 'parent'
+            })
+          ],
+          onmove: (event) => {
+            const target = event.target;
+            const x = parseFloat(target.getAttribute('x')) || 0;
+            const y = parseFloat(target.getAttribute('y')) || 0;
+
+            target.style.transform = `translate(${x + event.dx}px, ${y + event.dy}px)`;
+            target.setAttribute('x', x + event.dx);
+            target.setAttribute('y', y + event.dy);
+            gate.x = x + event.dx
+            gate.y = y + event.dy
+          },
+        });
+      }
+    });
+  }, [gates, selectedTool]);
 
   return (
     <div className='Playground'>
@@ -76,12 +75,12 @@ useEffect(() => {
         <button onClick={() => addGate('or')}>Add or</button>
         <button onClick={() => addGate('not')}>Add not</button>
       </div>
-      <Canvas/>
+      <Canvas />
       <div className='Toolbar'>
-      <button className={selectedTool === 'connect' ? 'SelectedTool' : ''} onClick={() => handleToolClick('connect')}>Connect</button>
-      <button className={selectedTool === 'erase' ? 'SelectedTool' : ''} onClick={() => handleToolClick('erase')}>Erase</button>
-      <button className={selectedTool === 'move' ? 'SelectedTool' : ''} onClick={() => handleToolClick('move')}>Move</button>
-    </div>
+        <button className={selectedTool === 'connect' ? 'SelectedTool' : ''} onClick={() => handleToolClick('connect')}>Connect</button>
+        <button className={selectedTool === 'erase' ? 'SelectedTool' : ''} onClick={() => handleToolClick('erase')}>Erase</button>
+        <button className={selectedTool === 'move' ? 'SelectedTool' : ''} onClick={() => handleToolClick('move')}>Move</button>
+      </div>
     </div>
   );
 }
