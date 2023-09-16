7b06d70b9053c0e93e7009feb436ca9db91723cd
SarahBerry01 <sarah@berrys.me.uk>
2023-09-16 11:30:54 +0100

    add moveable and gate

--- src/components/playground.js
+++ src/components/playground.js
@@ -1,46 +1,84 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect} from 'react';
+import interact from 'interactjs';
+import and from '../assets/and.svg'
 
 
 function Playground() {
-
-    const [gates, setGates] = useState([]);
-    class Gate {
-        constructor(type) {
-          this.type = type
-        }
-      }
-    
-    function GatePicker() {
-        return <div className="GatePicker">
-        <button className="GateButton" onClick={() => addGate({ type: "and" })}>AND</button>
-        <button className="GateButton" onClick={() => addGate({ type: "not" })}>NOT</button>
-        <button className="GateButton" onClick={() => addGate({ type: "or" })}>OR</button>
-        </div>
-    }
     function Canvas(){
-        return <div className="Canvas">
-        </div>
-    }
-    
-    function Toolbar() {
-        return <div className="Toolbar">
-        </div>
-    }
-    
-    function addGate(props) {
-        const newGate = new Gate(props.type)
-        console.log(props.type)
-        console.log(gates)
-        setGates([...gates, newGate]);
-      };
+        return <div className="Canvas" id="Canvas">
+        <svg src="../assets/and.svg"/>
+        {gates.map((gate) => (
+          <div
+            key={gate.id}
+            id={gate.id}
+            className={`gate ${gate.type}`}
+            x={gate.x}
+            y={gate.y}
+            style={{ transform: `translate(${gate.x}px, ${gate.y}px)` }}
+          ></div>
+        ))}
+      </div>
 
-      
-    return(
+    }
+  const [gates, setGates] = useState([]);
+
+  const addGate = (gateType) => {
+    const newGate = {
+      type: gateType,
+      id: Date.now(), // Use a unique identifier
+      x: 0, // Initial X position
+      y: 0, // Initial Y position
+    };
+
+    setGates([...gates, newGate]);
+  };
+
+
+useEffect(() => {
+
+
+  gates.forEach((gate) => {
+    const element = document.getElementById(gate.id);
+
+    if (element) {
+      interact(element).draggable({
+        modifiers: [
+          interact.modifiers.restrictRect({
+            restriction: 'parent'
+          })
+        ],
+        onmove: (event) => {
+          const target = event.target;
+          const x = parseFloat(target.getAttribute('x')) || 0;
+          const y = parseFloat(target.getAttribute('y')) || 0;
+  
+          target.style.transform = `translate(${x + event.dx}px, ${y + event.dy}px)`;
+          target.setAttribute('x', x + event.dx);
+          target.setAttribute('y', y + event.dy);
+          gate.x = x + event.dx
+          gate.y = y + event.dy
+        },
+      });
+    }
+  });
+}, [gates]);
+
+  return (
     <div className='Playground'>
-        <GatePicker/>
-        <Canvas/>
-        <Toolbar/>
-      </div>)
+      <div className='Toolbar'>
+        <button onClick={() => addGate('and')}>Add and</button>
+        <button onClick={() => addGate('or')}>Add or</button>
+        <button onClick={() => addGate('not')}>Add not</button>
+      </div>
+      <Canvas/>
+      <div className='Toolbar'>
+      <button>Connect</button>
+      <button>Erase</button>
+      <button>Move</button>
+    </div>
+    </div>
+  );
 }
+
 
 export default Playground;
