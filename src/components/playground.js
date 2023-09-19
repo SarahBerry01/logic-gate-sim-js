519e6b4fe34cd9b4a7bb047a32e6c50ef19083a1
SarahBerry01 <sarah@berrys.me.uk>
2023-09-19 08:01:43 +0100

    remove logs

--- src/components/playground.js
+++ src/components/playground.js
@@ -51,101 +51,127 @@ function Playground() {
     // Loop through the selected elements
     elements.forEach((element) => {
       if (element.classList.contains("gate")) {
-        console.log(element)
         if (element.getAttribute("inZeroRow")) {
-          let x1 = scale * (parseInt(element.getAttribute("col"))) * gateDim + 20;
-          let y1 = scale * (parseInt(element.getAttribute("row")) + (1/3)) * gateDim;
-          let x2 = scale * (parseInt(element.getAttribute("inZeroCol")) + 1) * gateDim;
-          let y2 = scale * (parseInt(element.getAttribute("inZeroRow")) +0.5)  * gateDim;
-          console.log(element.classList)
-          if (element.classList.contains("not") || element.classList.contains("output")){
-            console.log("!!!!!!!!!!!!")
-            y1 = scale * (parseInt(element.getAttribute("row")) + 0.5) * gateDim;
+          let x1 = scale * parseInt(element.getAttribute("col")) * gateDim + 20;
+          let y1 =
+            scale * (parseInt(element.getAttribute("row")) + 1 / 3) * gateDim;
+          let x2 =
+            scale * (parseInt(element.getAttribute("inZeroCol")) + 1) * gateDim;
+          let y2 =
+            scale *
+            (parseInt(element.getAttribute("inZeroRow")) + 0.5) *
+            gateDim;
+          if (
+            element.classList.contains("not") ||
+            element.classList.contains("output")
+          ) {
+            y1 =
+              scale * (parseInt(element.getAttribute("row")) + 0.5) * gateDim;
           }
-          draw(ctx, x1, y1, x2, y2)
+          draw(ctx, x1, y1, x2, y2);
         }
         if (element.getAttribute("inOneRow")) {
-          const x1 = scale * (parseInt(element.getAttribute("col"))) * gateDim;
-          const y1 = scale * (parseInt(element.getAttribute("row")) + (2/3)) * gateDim;
-          const x2 = scale * (parseInt(element.getAttribute("inOneCol")) + 1) * gateDim;
-          const y2 = scale * (parseInt(element.getAttribute("inOneRow")) +0.5)  * gateDim;
-          draw(ctx, x1, y1, x2, y2)
+          const x1 = scale * parseInt(element.getAttribute("col")) * gateDim;
+          const y1 =
+            scale * (parseInt(element.getAttribute("row")) + 2 / 3) * gateDim;
+          const x2 =
+            scale * (parseInt(element.getAttribute("inOneCol")) + 1) * gateDim;
+          const y2 =
+            scale *
+            (parseInt(element.getAttribute("inOneRow")) + 0.5) *
+            gateDim;
+          draw(ctx, x1, y1, x2, y2);
         }
       }
     });
   });
 
-  function draw(ctx, x1, y1, x2, y2){
+  function draw(ctx, x1, y1, x2, y2) {
     ctx.beginPath();
     ctx.moveTo(x1, y1);
-    ctx.lineTo((x1 + x2)/2, y1);
-    ctx.lineTo((x1 + x2)/2, y2);
+    ctx.lineTo((x1 + x2) / 2, y1);
+    ctx.lineTo((x1 + x2) / 2, y2);
     ctx.lineTo(x2, y2);
     ctx.stroke();
   }
 
-  function clear(){
+  function clear() {
     window.location.reload();
   }
 
-  function handleClick(row, col) {
-    const element = document.querySelector(`[row="${row}"][col="${col}"]`);
-    if (selectedTool === "select"){
-      console.log("switch");
-      if (element.classList.contains("switchOff")){
+  function handleSwitch(element) {
+    if (selectedTool === "select") {
+      if (element.classList.contains("switchOff")) {
         element.classList.remove("switchOff");
-        element.classList.add("switchOn")
+        element.classList.add("switchOn");
         element.setAttribute("gate", "switchOn");
-        console.log(element);
-      }
-      else if (element.classList.contains("switchOn")){
+      } else if (element.classList.contains("switchOn")) {
         element.classList.remove("switchOn");
-        element.classList.add("switchOff")
+        element.classList.add("switchOff");
         element.setAttribute("gate", "switchOff");
       }
+    }
+  }
+  function handleErase(element){
+  if (selectedTool === "erase") {
+    element.setAttribute("gate", null);
+    element.setAttribute("class", `gridElement`);
+    //add logic to remove connections
+  }}
 
-    }
-    if (selectedTool === "erase") {
-      element.setAttribute("gate", null);
-      element.setAttribute("class", `gridElement`);
-      //add logic to remove connections
-    }
-    if (selectedTool === "connect") {
-      if (selectedGate === undefined ) {
-        if(element.className !== "gridElement" && !element.classList.contains("output")){
-        console.log("a")
-        setSelectedGate({
-          row: element.getAttribute("row"),
-          col: element.getAttribute("col"),
-        });
-        element.classList.add("selectedGate")};
-      } else if (element.className !== "gridElement" ) {
-        console.log(element.inzerorow)
-        console.log("b")
-        // if no inputs exist, connect to top input
-        if (!element.getAttribute("inZeroRow")){
-        element.setAttribute("inZeroRow", selectedGate.row);
-        element.setAttribute("inZeroCol", selectedGate.col);
-        removeSelection();}
-        // if top input exists, connect to bottom input
-        else if (!element.inOneRow && !element.classList.contains("not")){
-          element.setAttribute("inOneRow", selectedGate.row);
-          element.setAttribute("inOneCol", selectedGate.col);
-          removeSelection();
-        }
-      }
-    }
+  function handleAddGate(element){
     if (["and", "or", "not", "switchOff", "output"].includes(selectedTool)) {
       element.setAttribute("gate", selectedTool);
       element.setAttribute("class", `gridElement gate ${selectedTool}`);
     }
   }
 
+  function handleConnect(element){
+    if (selectedTool === "connect") {
+      if (selectedGate === undefined) {
+        if (
+          element.className !== "gridElement" &&
+          !element.classList.contains("output")
+        ) {
+          setSelectedGate({
+            row: element.getAttribute("row"),
+            col: element.getAttribute("col"),
+          });
+          element.classList.add("selectedGate");
+        }
+      } else if (element.className !== "gridElement") {
+        // if no inputs exist, connect to top input
+        if (!element.getAttribute("inZeroRow")) {
+          element.setAttribute("inZeroRow", selectedGate.row);
+          element.setAttribute("inZeroCol", selectedGate.col);
+          removeSelection();
+        }
+        // if top input exists, connect to bottom input
+        else if (!element.inOneRow && !element.classList.contains("not")) {
+          element.setAttribute("inOneRow", selectedGate.row);
+          element.setAttribute("inOneCol", selectedGate.col);
+          removeSelection();
+        }
+      }
+    }
+  }
+
+  function handleClick(row, col) {
+    const element = document.querySelector(`[row="${row}"][col="${col}"]`);
+    handleSwitch(element);
+    handleErase(element);
+    handleAddGate(element);
+    handleConnect(element);
+
+  }
+
   return (
     <div className="Playground">
       <div className="Toolbar">
-      <button
-          className={selectedTool === "switchOff" ? "tool SelectedTool" : "tool"}
+        <button
+          className={
+            selectedTool === "switchOff" ? "tool SelectedTool" : "tool"
+          }
           onClick={() => handleToolClick("switchOff")}
         >
           Add switch
@@ -192,18 +218,13 @@ function Playground() {
         >
           select
         </button>
-        <button
-          className={"tool"}
-          onClick={() => clear()}
-        >
+        <button className={"tool"} onClick={() => clear()}>
           clear
         </button>
       </div>
       <div className="circuitPlayground">
-        <div class="gridContainer">
-        {generateGrid()}
-      </div>
-      <canvas id="canvas" width="6400" height="4800" />
+        <div class="gridContainer">{generateGrid()}</div>
+        <canvas id="canvas" width="6400" height="4800" />
       </div>
     </div>
   );
